bceb7aa257ecb062821840a0641a0223e0e4cac5
laithba <laithmousa035@gmail.com>
2025-10-08 13:34:35 +0300

    Update vite config and fix GitHub Pages path

--- src/Componants/Nav/Nav.jsx
+++ src/Componants/Nav/Nav.jsx
@@ -32,14 +32,15 @@ function Nav() {
     <nav className="navbar">
       <div className="logo">Laith</div>
 
-      <ul ref={menuRef} className={`nav-links ${isOpen ? "active" : ""}`}>
-        <li><a href="#home">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#skills">Skills</a></li>
-        <li><a href="#education">Education</a></li>
-        <li><a href="#projects">Projects</a></li>
-        <li><a href="#contact">Contact</a></li>
-      </ul>
+     <ul ref={menuRef} className={`nav-links ${isOpen ? "active" : ""}`}>
+  <li><a href="#home" onClick={() => setIsOpen(false)}>Home</a></li>
+  <li><a href="#about" onClick={() => setIsOpen(false)}>About</a></li>
+  <li><a href="#skills" onClick={() => setIsOpen(false)}>Skills</a></li>
+  <li><a href="#education" onClick={() => setIsOpen(false)}>Education</a></li>
+  <li><a href="#projects" onClick={() => setIsOpen(false)}>Projects</a></li>
+  <li><a href="#contact" onClick={() => setIsOpen(false)}>Contact</a></li>
+</ul>
+
 
       <div className="burger" onClick={toggleMenu}>
         <div className="line"></div>
